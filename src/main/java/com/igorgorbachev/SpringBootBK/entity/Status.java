ab3c7036ca632002a6271dfa08a8930707d794f5
IgorGorbachev <ronjke9090@mail.ru>
2025-09-03 19:17:28 +0300

    done entity

--- src/main/java/com/igorgorbachev/SpringBootBK/entity/Status.java
+++ src/main/java/com/igorgorbachev/SpringBootBK/entity/Status.java
@@ -1,59 +1,49 @@
 package com.igorgorbachev.SpringBootBK.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
-@Table
+@Table(name = "status")
 public class Status {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     Long id;
 
+    @Column(name = "name_status")
     String nameStatus;
-
-    public Status() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNameStatus() {
-        return nameStatus;
-    }
-
-    public void setNameStatus(String nameStatus) {
-        this.nameStatus = nameStatus;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Status status = (Status) o;
-        return Objects.equals(id, status.id) && Objects.equals(nameStatus, status.nameStatus);
+        return Objects.equals(id, status.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nameStatus);
+        return Objects.hash(id);
     }
 
     @Override
     public String toString() {
         return "Status{" +
-               "id=" + id +
-               ", nameStatus='" + nameStatus + '\'' +
-               '}';
+                "id=" + id +
+                ", nameStatus='" + nameStatus + '\'' +
+                '}';
     }
 }
--- src/main/java/com/igorgorbachev/SpringBootBK/entity/Task.java
+++ src/main/java/com/igorgorbachev/SpringBootBK/entity/Task.java
@@ -5,54 +5,44 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "task")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "text")
     private String text;
 
-    public Task() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return Objects.equals(id, task.id) && Objects.equals(text, task.text);
+        return Objects.equals(id, task.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, text);
+        return Objects.hash(id);
     }
 
     @Override
     public String toString() {
         return "Task{" +
-               "id=" + id +
-               ", text='" + text + '\'' +
-               '}';
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
     }
 }
